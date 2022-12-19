e1e0ece3bae0bf9917060fb845fe217276278312
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-19 23:01:31 +0900

    #9.7 Slider part One
    ## 목표
    
    - Home 컴포넌트에 있는 Slider 를 구현해본다.
        - framer motion 의 Slider 예제와는 약간 다르게 여러 열로 이루어져 있다.
            - 추후 응용이 가능하도록 여러 열 Slider의 구조와 구현 방법을 이해한다.
    
    ## 구현
    
    - framer motion 을 사용해서 Slider를 구현한다.
    - `AnimatePresence` 와 `Array.map()` 을 사용해서 순환문 형식으로 Rendering 한다.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getMovies, IGetMoviesResult } from '../api';
 import { makeImagePath } from '../utils';
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   background-color: black;
@@ -31,21 +33,76 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ['movies', 'nowPlaying'],
     getMovies,
   );
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
 
   return (
     <Wrapper style={{ backgroundColor: 'whitesmoke', height: '200vh' }}>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}>
-          <Title>{data?.results[0].title}</Title>
-          <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
+        <>
+          <Banner
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}
+            onClick={incraseIndex}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: 'tween', duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
       )}
     </Wrapper>
   );
