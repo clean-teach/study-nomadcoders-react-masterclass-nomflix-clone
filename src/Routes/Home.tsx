9bebd78f22f65320374f6947bffd26e9f0145eeb
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-20 00:31:38 +0900

    #9.10 Box Animations part Two
    ### Hover 시 info 영역 나타나기
    
    - `variants` 를 갖는 부모와 자식 엘리먼트가 있다면, 부모 엘리먼트의 `whileHover` 와 같은 Props 들이 자동으로 자식에게 상속 된다.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -63,6 +63,19 @@ const Box = styled(motion.div)<{ $bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + 5,
@@ -81,10 +94,21 @@ const boxVariants = {
   },
   hover: {
     scale: 1.3,
-    y: -50,
+    y: -80,
     transition: {
       delay: 0.5,
-      duaration: 0.3,
+      duaration: 0.1,
+      type: 'tween',
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
       type: 'tween',
     },
   },
@@ -144,7 +168,11 @@ function Home() {
                       initial="normal"
                       variants={boxVariants}
                       transition={{ type: 'tween' }}
-                    />
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
